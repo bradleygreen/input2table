397a4a63919db1b1d34a39f4ab52315f49e5775e
Brad Green <bradgreen@infowest.com>
2014-04-01 09:27:12 -0600

    added sameLineSplit function, csvSplit function and displayTable function

--- input2table.js
+++ input2table.js
@@ -13,3 +13,46 @@ window.onload = function()
 		displayTable();
 	}
 }
+
+/* sameLineSplit()function
+ * purpose: split two-dimensional array into usable pieces.
+ * parameters: (string) data from textarea
+ * returns: (array(array)) two-dimensional array of data*/
+function sameLineSplit(data)
+{
+	console.log("the sameLineSplit(data) function fired");
+	var newLineArray = data.split("\n");
+	return (newLineArray);
+}
+
+/* csvSplit()function
+ * purpose: split two-dimensional array into usable pieces.
+ * parameters: line of array from the sameLineSplit function
+ * returns: array of one state, one year, and one motto*/
+function csvSplit(lines)
+{
+	console.log("the csvSplit(lines) function fired");
+	var newCSV_Array = lines.split(",");
+	return (newCSV_Array);
+}
+
+/* displayData function (callback function)
+ * purpose: harvest data & display it
+ * parameters: none
+ * returns: nothing*/
+function displayTable()
+{
+	console.log("the displayTable() function fired");
+	var data = document.getElementById("inputArea").value;
+	var tempLineArray = sameLineSplit(data);
+	
+	for(var index = 0; index < tempLineArray.length; index++)
+	{
+		console.log("the displayTable() function's first for loop fired");
+		var tempCsvArray = csvSplit(tempLineArray[index]);
+		console.log("the displayStates() function's for(for(csvIndex)) fired");
+		document.getElementById("jsOutput").innerHTML = document.getElementById("jsOutput").innerHTML + "<table>";
+		
+		// old function + "You have lived in " + tempCsvArray[0] + ", which gained statehood in " + tempCsvArray[1] + " and has the motto: \"" + tempCsvArray[2] + "\".<br /><br />";
+	}
+}
