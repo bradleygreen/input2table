85db6d6270c031eae05b08e8083478e085382cbb
Brad Green <bradgreen@infowest.com>
2014-04-01 13:05:00 -0600

    built error checking for lineSplit function, and catch for its caller

--- input2table.js
+++ input2table.js
@@ -21,8 +21,25 @@ window.onload = function()
 function sameLineSplit(data)
 {
 	console.log("the sameLineSplit(data) function fired");
-	var newLineArray = data.split("\n");
-	return (newLineArray);
+	var dirtyLineArray = data.split("\n");
+	var cleanLineArray =  new Array ();
+	var lineException = "";
+	for(var lineIndex = 0; lineIndex < dirtyLineArray.length; lineIndex++)
+	{
+		console.log("the sameLineSplit(data) function's for loop fired");
+		if(dirtyLineArray[lineIndex] == "")
+		{
+			lineException += "Line " + (lineIndex+1) + " was empty, skipping<br />";
+		}
+		else
+		{
+			cleanLineArray.push(dirtyLineArray[lineIndex]);
+		}
+	}
+	var mixedLineArray = new Array();
+	mixedLineArray.push(lineException);
+	mixedLineArray.push(cleanLineArray);
+	return (mixedLineArray);
 }
 
 /* csvSplit()function
@@ -45,14 +62,33 @@ function displayTable()
 	console.log("the displayTable() function fired");
 	var newHTMLTable = "";
 	var data = document.getElementById("inputArea").value; //get information entered by user
-	var tempLineArray = sameLineSplit(data); //call sameLineSplit function, handing it data and getting back an array
+	try
+	{
+		var tempLineArray = sameLineSplit(data); //call sameLineSplit function, handing it data and getting back an array
+	}
+	catch(mixedLineArray)
+	{
+		document.getElementById("jsExceptions").innerHTML = document.getElementById("jsExceptions").innerHTML + mixedLineArray[0];
+		tempLineArray = mixedLineArray[1];
+	}
+	
 	//create the header row (will be the same every time)
 	newHTMLTable += "<table><tr><th>Standard</th><th>Version</th><th>Date</th></tr>";
 	
 	for(var index = 0; index < tempLineArray.length; index++)
 	{
 		console.log("the displayTable() function's first for loop fired");
-		var tempCsvArray = csvSplit(tempLineArray[index]);
+		
+		try
+		{
+			var tempCsvArray = csvSplit(tempLineArray[index]);
+		}
+		catch(mixedCsvArray)
+		{
+			document.getElementById("jsExceptions").innerHTML = document.getElementById("jsExceptions").innerHTML + mixedCsvArray[0];
+			studentArray = mixedCsvArray[1];
+		}
+		
 		newHTMLTable += "<tr><td>" + tempCsvArray[0] + "</td><td>" + tempCsvArray[1] + "</td><td>" + tempCsvArray[2] + "</td></tr>";
 		
 		// old function + "You have lived in " + tempCsvArray[0] + ", which gained statehood in " + tempCsvArray[1] + " and has the motto: \"" + tempCsvArray[2] + "\".<br /><br />";
@@ -62,13 +98,27 @@ function displayTable()
 	document.getElementById("jsOutput").innerHTML = newHTMLTable;
 }
 
-/* exception code for user entering blank data or if the version 
- * number is ≤ 0, throw exception */
+/* 
+// create a Java-style exception if we had errors
+	if(numExceptions>0)
+	{
+			var javaException =  new Array ();
+			javaException.push(numExceptions+" blank names detected"); // first element:error message for the userAgent
+			javaException.push(studentArray); //second element: cleaned student array
+			throw(javaException); //now throw both things to caller
+	}			
+	else
+	{ 
+		return(versionsArrays);
+	}
 
+exception code for user entering blank data or if the version 
+ * number is ≤ 0, throw exception */
+/*
 function versionsArrays()
 {
-	/* Exception inside the callback function - harvest the text 
-	 * area data & convert data */
+	// Exception inside the callback function - harvest the text 
+	// area data & convert data 
 	try
 	{
 		var data = document.getElementById("inputArea").value;
@@ -85,16 +135,16 @@ function versionsArrays()
 	//print each version, line by line
 	for(var index = 0; index<versionsArrays.length;index++)
 	{
-		document.getElementById("jsOutput").innerHTML=document.getElementById("jsOutput").innerHTML+versionsArray[index]/*references array name*/+ " attended class"+"<br/>";
+		document.getElementById("jsOutput").innerHTML=document.getElementById("jsOutput").innerHTML+versionsArray[index]/*references array name+ " attended class"+"<br/>";
 	}
 	 
-}
+}*/
 	
 
 /* Exception inside the data from the text area which 
  * converts it to an array 
  *input: (string) data from text area
- *output: (data to table)*/
+ *output: (data to table)
  
  function convertTextareaToTable(data)
 {
@@ -117,16 +167,5 @@ function versionsArrays()
 }
 		
 	
-// create a Java-style exception if we had errors
-	if(numExceptions>0)
-	{
-			var javaException =  new Array ();
-			javaException.push(numExceptions+" blank names detected"); // first element:error message for the userAgent
-			javaException.push(studentArray); //second element: cleaned student array
-			throw(javaException); //now throw both things to caller
-	}			
-	else
-	{ 
-		return(versionsArrays);
-	}
-	
+
+	*/
